104f9564df4bb4637f0fe990e3114305b532929d
Olivier Bourgeois <obourgeois@alteca.fr>
2024-05-21 14:13:11 +0200

    boucles dans le DOM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,27 @@ export class AppComponent {
 
   paragrapheId = 0;
 
+  cars = [
+    {
+      id: 0,
+      brand: 'Renault',
+      model: 'Laguna',
+      color: 'gray',
+    },
+    {
+      id: 1,
+      brand: 'Peugeot',
+      model: '508',
+      color: 'red',
+    },
+    {
+      id: 2,
+      brand: 'Opel',
+      model: 'Corsa',
+      color: 'blue',
+    },
+  ];
+
   onClickButton(): void {
     console.log('Hello World');
     this.displayText = this.displayText ? false : true;
