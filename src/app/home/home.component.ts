fb841efdc78c9b7a54c7632dc9162007d4662489
Olivier Bourgeois <obourgeois@alteca.fr>
2024-05-22 15:05:47 +0200

    redirection

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  onLogin(): void {
+    // this.router.navigate(['/admin', 'dashboard']);
+    this.router.navigateByUrl('/admin/dashboard');
+  }
 }
