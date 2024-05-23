4fb0d14b9684c49402123518c8ed81327ea471ca
Olivier Bourgeois <obourgeois@alteca.fr>
2024-05-23 09:05:29 +0200

    mise en place de l'interface admin : modifier des annonces

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -19,6 +19,7 @@ export class DashboardComponent implements OnInit {
 
   initOfferForm(): void {
     this.offerForm = this.formBuilder.group({
+      index: [0],
       title: ['', [Validators.required, Validators.maxLength(100)]],
       brand: '',
       model: '',
@@ -28,8 +29,27 @@ export class DashboardComponent implements OnInit {
   }
   onSubmitOfferForm(): void {
     console.log(this.offerForm.invalid);
-    this.offers.push(this.offerForm.value);
+    // on récupère l'index du formulaire
+    const offerIndex = this.offerForm.value.index;
+    // on récupère les info de l'élément
+    let offer = this.offerForm.value;
+
+    if (offerIndex == null || offerIndex == undefined) {
+      delete offer.index;
+      this.offers.push(offer);
+    } else {
+      delete offer.index;
+      // on modifie l'élément
+      this.offers[offerIndex] = offer;
+    }
+
+    // this.offers.push(this.offerForm.value);
     this.offerForm.reset();
     console.log(this.offers);
+  }
+
+  onEditOffer(offer: any, index: number): void {
+    // les  ... pour déconstruire l'élément
+    this.offerForm.setValue({ ...offer, index });
   }
 }
